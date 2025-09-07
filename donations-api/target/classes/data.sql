b8d635b48eed684ec1164b6195f0b1c563d2c634
wagnermfds <wagnermfds@gmail.com>
2025-09-07 10:49:01 -0300

    adicionando alteracoes

---
+++ donations-api/target/classes/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, password, role) VALUES
+('Carla Martins', 'carla@email.com', '123456', 'DONOR'),
+('Dona Lúcia Ferreira', 'lucia@email.com', '123456', 'BENEFICIARY'),
+('ONG Esperança', 'ong@email.com', '123456', 'ONG');
