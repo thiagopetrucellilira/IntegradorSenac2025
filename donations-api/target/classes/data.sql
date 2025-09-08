fcf2875272f2aef68b7f7833c446982de8a52212
iagopedro <iagopedroltd19@gmail.com>
2025-09-08 13:32:55 -0300

    ajustes para integração entre api e bd, adiciona git ignore e setup do mysql

--- donations-api/target/classes/data.sql
+++ donations-api/target/classes/data.sql
@@ -1,4 +1,13 @@
 INSERT INTO users (name, email, password, role) VALUES
-('Carla Martins', 'carla@email.com', '123456', 'DONOR'),
-('Dona Lúcia Ferreira', 'lucia@email.com', '123456', 'BENEFICIARY'),
-('ONG Esperança', 'ong@email.com', '123456', 'ONG');
+('Carla Martins', 'carlamar@email.com', '123456', 'DONOR'),
+('Dona Lúcia Ferreira', 'luciaferrra@email.com', '123456', 'BENEFICIARY'),
+('ONG Esperança', 'ongesper@email.com', '123456', 'ONG'),
+('Pedro Pertruci', 'pedraoptc@email.com', '123456', 'DONOR'),
+('Rafael Yuiti', 'ryuitipkm@email.com', '123456', 'DONOR'),
+('Daniela Lima', 'danielaela@email.com', '123456', 'DONOR'),
+('João Fernando Lopez', 'jfernandoL@email.com', '123456', 'BENEFICIARY'),
+('Maya Luna Silva', 'mayaluna@email.com', '123456', 'BENEFICIARY'),
+('Reinaldo Dino Paiva', 'reinaldodpaiva@email.com', '123456', 'BENEFICIARY'),
+('Organização Alimente todos', 'orgalimentodos@email.com', '123456', 'ONG'),
+('Movimento da Educação Universal', 'moveduniversal@email.com', '123456', 'ONG'),
+('Tecnologia Acessivel', 'ecnologiaacessive@email.com', '123456', 'ONG');
